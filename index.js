0595f2d9ff88833b7b59fab67d75a0a34fc7f35e
Medhaja H L <medhajahlbhat@gmail.com>
2022-01-10 02:15:21 +0530

    ae

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ var player = document.getElementById("player");
       captureButton.addEventListener("click", async function () {
         // Load the model.
         // net = await mobilenet.load();
-        const ASSETS_URL = `${window.location.origin}`
+        const ASSETS_URL = `https://github.com/medhaja/test2`
         const WEIGHTS_URL = `${ASSETS_URL}/Weights/model.json`
         net = await tf.loadLayersModel(WEIGHTS_URL);
         console.log('Model loaded!!');
